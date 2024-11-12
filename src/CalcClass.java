9177573f9cf0a270c687ae1b9a6eb6ba0c3f283c
Ryszard Różański <rychu48@op.pl>
2024-11-12 22:06:40 +0100

    conflictsssss test

---
+++ src/CalcClass.java
@@ -0,0 +1,20 @@
+package src;
+
+public class CalcClass {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
